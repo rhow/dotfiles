fb39572965e630be22f696307ddb84b05be47958
Randy How <rhow@i3.com>
2016-01-03 13:22:33 -0700

    Better search for MEM and CPU

--- bin/running_vagrants.sh
+++ bin/running_vagrants.sh
@@ -1,8 +1,8 @@
 
 for i in $(vagrant global-status | ag running | awk '{print $5}'); do 
     VAGRANTFILE=$i/Vagrantfile
-    V_MEMORY=$(grep 'vb.memory' $VAGRANTFILE)
-    V_CPU=$(grep 'vb.cpu' $VAGRANTFILE)
+    V_MEMORY=$(grep -i 'memory[ +]' $VAGRANTFILE | grep -v '#')
+    V_CPU=$(grep -i 'cpus[ +]' $VAGRANTFILE)
     
     echo $V_MEMORY";" $V_CPU";" $VAGRANTFILE
 done
