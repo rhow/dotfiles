be2ebdb702ddbf53fc5cbcec2125286d8329fe5d
Randy How <rhow@i3.com>
2017-05-24 14:33:14 -0600

    Use 3.5 and add activate symlink

--- bin/py34virtenv_setup.sh
+++ bin/py34virtenv_setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-python3.4 -m venv --without-pip virtualenv
-source ./virtualenv/bin/activate
+# set -ex
+
+python3.5 -m venv --without-pip virtualenv
+ln -s `pwd`/virtualenv/bin/activate
+source `pwd`/virtualenv/bin/activate
 curl 'https://bootstrap.pypa.io/get-pip.py' | python
+
+echo -e '\nActivate new virtual env with: . ./activate'
