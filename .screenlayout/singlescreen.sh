ea103e4edcc7290df3d7068f6a7badc153ff703c
Randy How <rhow@airbus-na.com>
2018-05-18 17:07:25 -0600

    Initial commit

---
+++ .screenlayout/singlescreen.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-2-1 --off --output DP-2-2 --off --output DP-2-3 --off --output eDP1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2 --off --output DP-1 --off
