92aec0378031dec94500f567df710023d1001109
Randy How <rhow@airbus-na.com>
2018-06-05 12:42:11 -0600

    Updated with new laptop

--- .screenlayout/dualscreen.sh
+++ .screenlayout/dualscreen.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP1 --off --output DP2-1 --off --output DP2-2 --mode 1920x1200 --pos 0x0 --rotate normal --output DP2-3 --off --output DP1-3 --off --output DP1-2 --off --output DP1-1 --off --output DP2 --off
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1600x900 --pos 1920x0 --rotate normal --output DP1 --off --output DP2-1 --off --output DP2-2 --mode 1920x1200 --pos 0x0 --rotate normal --output DP2-3 --off --output DP2 --off
--- .screenlayout/singlescreen.sh
+++ .screenlayout/singlescreen.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-2-1 --off --output DP-2-2 --off --output DP-2-3 --off --output eDP1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2 --off --output DP-1 --off
+xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP2-1 --off --output DP2-2 --off
