a023ad80b6de61051190663eba010ec46e32754c
Randy How <rhow@airbus-na.com>
2019-03-01 10:10:43 -0700

    Comment out DP1

--- .screenlayout/mirror-dp2.sh
+++ .screenlayout/mirror-dp2.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP2 --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+# xrandr --output DP1 --mode 1920x1200 --pos 0x0 --rotate normal --scale 1x1
+xrandr --output DP2 --mode 1920x1080 --pos 0x0 --rotate normal --scale 1x1
+# --scale 0.9x0.9
