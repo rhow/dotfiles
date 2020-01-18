c8b52e25c4da6de0ec223d93b16477fb7a102d38
Randy How <rhow@airbus-na.com>
2020-01-17 18:36:20 -0700

    Added various tunnels

--- bin/ssh_tunnels.sh
+++ bin/ssh_tunnels.sh
@@ -39,9 +39,20 @@ autossh ${USER}@bastion.us-central1-c.ads-ftc-noc -L 0.0.0.0:8383:10.128.0.251:8
 # QUAL DB
 autossh ${USER}@bastion.us-central1-b.datadoors-qual -L 0.0.0.0:5477:35.194.26.54:5432 -N -f
 
+# QUAL DDWebServerUtil loadbalancer
+autossh ${USER}@bastion.us-central1-b.datadoors-qual -L 0.0.0.0:8731:10.128.0.210:8731 -N -f
+
+# QUAL DataDoors Streaming Server
+autossh ${USER}@bastion.us-central1-b.datadoors-qual -L 0.0.0.0:6677:10.128.0.185:80 -N -f
+
+# DataDoors Streaming Authoring API
+autossh ${USER}@bastion.us-central1-b.datadoors-qual -L 0.0.0.0:8866:10.128.0.255:8080 -N -f
+autossh ${USER}@bastion.us-central1-c.ads-ftc-noc -L 0.0.0.0:8865:10.128.0.255:8080 -N -f
+
 # Tensorboard
 autossh rhow@rhow-ml-vm.us-central1-a.ag-qual-frontend-adsftc -L 0.0.0.0:26006:10.128.0.19:6006 -N -f
 
 # OneAtlas
 autossh rhow@admin-lin.europe-west1-b.one-atlas-live-1334 -L 0.0.0.0:44444:192.168.10.3:5432 -N -f   	# database-2
 autossh rhow@admin-lin.europe-west1-b.one-atlas-dc-dev -L 0.0.0.0:44445:192.168.10.2:5432 -N -f 		# database-1
+
