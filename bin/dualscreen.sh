e30be0f802070179f895975fa9bd6a00d084800b
Randy How <rhow@airbus-na.com>
2023-04-27 16:31:42 -0600

    Updated

--- bin/dualscreen.sh
+++ bin/dualscreen.sh
@@ -4,7 +4,11 @@
 # xrandr --output eDP1 --mode 1600x900_60.00
 # xrandr --output HDMI1 --auto --left-of eDP1
 
-xrandr --newmode "1424x800" 93.25  1424 1504 1648 1872  800 803 813 831 -hsync +vsync
-xrandr --addmode eDP1  1424x800
-xrandr --output eDP1 --mode 1424x800
-xrandr --output HDMI1 --auto --left-of eDP1
+#xrandr --newmode "1424x800" 93.25  1424 1504 1648 1872  800 803 813 831 -hsync +vsync
+#xrandr --output eDP1 --mode 1424x800
+
+xrandr --output DP2-1 --auto
+xrandr --output DP2-3 --auto --right-of DP2-1
+xrandr --output eDP1 --auto --below DP2-1 --below DP2-3
+
+# xrandr --output DP2-3 "1920x1080"
--- bin/singlescreen.sh
+++ bin/singlescreen.sh
@@ -1,3 +1,5 @@
 # turns off the external monitor
 xrandr --output eDP1 --auto 
-xrandr --output HDMI1 --off 
+xrandr --output DP2-1 --off 
+xrandr --output DP2-3 --off 
+
